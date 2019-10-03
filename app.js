447751c1d0904978d3e3a81e326dffbeeb5e8a4b
asalanovich <anton.salanovich@vrpconsulting.com>
2019-10-03 17:16:47 +0300

    3nd commit

--- app.js
+++ app.js
@@ -6,28 +6,27 @@ const app = express();
 var sf = require('node-salesforce');
 
 
-
-app.use( bodyParcer.urlencoded({extended: true}) );
-app.use( bodyParcer.json() );
+app.use(bodyParcer.urlencoded({extended: true}));
+app.use(bodyParcer.json());
 
 var posts = [
-    {title : "My First post 1", content: "1wdaedawdawdawdawd"},
-    {title : "My First post 2", content: "22222222222wd"},
-    {title : "My First post 3", content: "333333333dawdawdawdawd"}
+    {title: "My First post 1", content: "1wdaedawdawdawdawd"},
+    {title: "My First post 2", content: "22222222222wd"},
+    {title: "My First post 3", content: "333333333dawdawdawdawd"}
 ];
 
 var oauth2 = new sf.OAuth2({
     // you can change loginUrl to connect to sandbox or prerelease env.
-    loginUrl : 'https://login.salesforce.com',
-    clientId : '3MVG9G9pzCUSkzZtjYVaRpHLTcvjkathl2nrl06zSaA5h6iTagnQJ0ktvCO5gpluAXPC84xas_K.zEHW_oynd',
-    clientSecret : 'F31069EADF519941FEC58E9FDE88FA51D768492FF1A3034C63339D7FB3F3FACE',
-    redirectUri : 'https://newsite1.herokuapp.com/callback'
+    loginUrl: 'https://login.salesforce.com',
+    clientId: '3MVG9G9pzCUSkzZtjYVaRpHLTcvjkathl2nrl06zSaA5h6iTagnQJ0ktvCO5gpluAXPC84xas_K.zEHW_oynd',
+    clientSecret: 'F31069EADF519941FEC58E9FDE88FA51D768492FF1A3034C63339D7FB3F3FACE',
+    redirectUri: 'https://newsite1.herokuapp.com/callback'
 });
 
-var conn = new sf.Connection({ oauth2 : oauth2 });
+var conn = new sf.Connection({oauth2: oauth2});
 
 app.get('/', function (req, res) {
-    res.render('index.ejs', { posts: posts } );
+    res.render('index.ejs', {posts: posts});
 });
 
 app.get('/oauth2/auth', function (req, res) {
@@ -41,18 +40,20 @@ app.get("/callback", function (req, res) {
 
 
     var code = req.param('code');
-/*
-    var oauth2 = new sf.OAuth2({
-        // you can change loginUrl to connect to sandbox or prerelease env.
-        loginUrl : 'https://login.salesforce.com',
-        clientId : '3MVG9G9pzCUSkzZtjYVaRpHLTcvjkathl2nrl06zSaA5h6iTagnQJ0ktvCO5gpluAXPC84xas_K.zEHW_oynd',
-        clientSecret : 'F31069EADF519941FEC58E9FDE88FA51D768492FF1A3034C63339D7FB3F3FACE',
-        redirectUri : 'https://newsite1.herokuapp.com/callback2'
-    }); */
-    var conn = new sf.Connection({ oauth2 : oauth2 });
+    /*
+        var oauth2 = new sf.OAuth2({
+            // you can change loginUrl to connect to sandbox or prerelease env.
+            loginUrl : 'https://login.salesforce.com',
+            clientId : '3MVG9G9pzCUSkzZtjYVaRpHLTcvjkathl2nrl06zSaA5h6iTagnQJ0ktvCO5gpluAXPC84xas_K.zEHW_oynd',
+            clientSecret : 'F31069EADF519941FEC58E9FDE88FA51D768492FF1A3034C63339D7FB3F3FACE',
+            redirectUri : 'https://newsite1.herokuapp.com/callback2'
+        }); */
+    var conn = new sf.Connection({oauth2: oauth2});
 
-    conn.authorize(code, function(err, userInfo) {
-        if (err) { return console.error(err); }
+    conn.authorize(code, function (err, userInfo) {
+        if (err) {
+            return console.error(err);
+        }
         // Now you can get the access token, refresh token, and instance URL information.
         // Save them to establish connection next time.
         console.log(conn.accessToken);
@@ -60,22 +61,28 @@ app.get("/callback", function (req, res) {
         console.log(conn.instanceUrl);
         console.log("User ID: " + userInfo.id);
         console.log("Org ID: " + userInfo.organizationId);
-        res.render('post.ejs', { post: "You connection SUCCESS" } );
+
+        var accessToken = conn.accessToken;
+        var auhorization = 'Bearer ' + accessToken;
+        var getUrl = conn.instanceUrl + '/services/apexrest/checkUserAccess';
+
+        request.get({
+            headers: {
+                'content-type': 'application/json'
+                'Authorization': auhorization
+            },
+            url: getUrl
+        }, function (error, response, body) {
+            console.log('bodyResp = ' + body);
+            console.log('Error = ' + error);
+            console.log('response = ' + response);
+
+            res.render('post.ejs', {post: "You connection SUCCESS " + response});
+        });
+
+
         // ...
     });
-
-   /* request.post({
-        headers: {'content-type' : 'application/json'},
-        url:     'http://login.salesforce.com/services/oauth2/token',
-        body:    body
-    }, function(error, response, body){
-        console.log('bodyResp = ' + body);
-        console.log('Error = ' + error);
-        console.log('response = ' + response);
-
-        res.render('post.ejs', { post: posts[1] } );
-    });*/
-
 
 
 });
